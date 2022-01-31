78f56c8c73af7e170f0b077cb1a59dbfa914501f
kpovel <kar.pavel123@gmail.com>
2022-01-31 18:48:35 +0200

    small fix

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function showVerticalMessage(str) {
     const sliceStr = str.slice(0, 10)
-    const registerConversion = (sliceStr[0] === 'м') ?  sliceStr[0].toUpperCase() + sliceStr.slice(1, 10) : sliceStr
+    const registerConversion = (sliceStr[0] === 'м') ? sliceStr[0].toUpperCase() + sliceStr.slice(1) : sliceStr
 
     for (let char of registerConversion) {
         console.log(char)
