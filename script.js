0592e4c62a8c7eb38e874c5d2bb9a45b3cea52cb
kpovel <kar.pavel123@gmail.com>
2022-01-31 17:30:51 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function showVerticalMessage(str) {
+    const sliceStr = str.slice(0, 10)
+    const registerConversion = (sliceStr[0] === 'м') ?  sliceStr[0].toUpperCase() + sliceStr.slice(1, 10) : sliceStr
+
+    for (let char of registerConversion) {
+        console.log(char)
+    }
+}
+
+showVerticalMessage('марафон')
